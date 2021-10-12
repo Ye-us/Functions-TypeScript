7500ae4a573345b0b2f2caa0056549d868463dd6
Manjiro <80841842+Manjiro-Sano@users.noreply.github.com>
2021-10-12 05:47:39 -0300

    Create CustomHooks.ts

---
+++ CustomHooks.ts
@@ -0,0 +1,8 @@
+export function useLoading() {
+  const [isLoading, setState] = React.useState(false);
+  const load = (aPromise: Promise<any>) => {
+    setState(true);
+    return aPromise.finally(() => setState(false));
+  };
+  return [isLoading, load] as const; // infers [boolean, typeof load] instead of (boolean | typeof load)[]
+}
